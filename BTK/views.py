13575dc3bf106043dc59f4ba889ad28911eac0bb
boylondon7 <gagalostone@gmail.com>
2025-02-14 23:10:25 +0000

    modifications sur les  urls d'ajout et de suppression de categories,paniers,Client,produits,Achats,Transactions

--- BTK/views.py
+++ BTK/views.py
@@ -1,5 +1,5 @@
-# Create your views here.
-from django.shortcuts import render, redirect
+
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Categorie, Produit, Client, Achat, PanierClient, Transaction, Facture
 
 # CATEGORIES
@@ -13,6 +13,20 @@ def ajout_categorie(request):
         Categorie.objects.create(nom=nom)
         return redirect('liste_categories')
     return render(request, 'categories/ajout.html')
+def modifier_categorie(request, pk):
+    categorie = get_object_or_404(Categorie, pk=pk)
+    if request.method == 'POST':
+        categorie.nom = request.POST['nom']
+        categorie.save()
+        return redirect('liste_categories')
+    return render(request, 'categories/modifier.html', {'categorie': categorie})
+
+def supprimer_categorie(request, pk):
+    categorie = get_object_or_404(Categorie, pk=pk)
+    if request.method == 'POST':
+        categorie.delete()
+        return redirect('liste_categories')
+    return render(request, 'categories/supprimer.html', {'categorie': categorie})
 
 # PRODUITS
 def liste_produits(request):
@@ -31,6 +45,26 @@ def ajout_produit(request):
         return redirect('liste_produits')
     categories = Categorie.objects.all()
     return render(request, 'produits/ajout.html', {'categories': categories})
+def modifier_produit(request, pk):
+    produit = get_object_or_404(Produit, pk=pk)
+    if request.method == 'POST':
+        produit.nom = request.POST['nom']
+        produit.prix = request.POST['prix']
+        produit.quantite = request.POST['quantite']
+        produit.description = request.POST['description']
+        produit.categorie = Categorie.objects.get(id=request.POST['categorie'])
+        produit.save()
+        return redirect('liste_produits')
+    categories = Categorie.objects.all()
+    return render(request, 'produits/modifier.html', {'produit': produit, 'categories': categories})
+
+def supprimer_produit(request, pk):
+    produit = get_object_or_404(Produit, pk=pk)
+    if request.method == 'POST':
+        produit.delete()
+        return redirect('liste_produits')
+    return render(request, 'produits/supprimer.html', {'produit': produit})
+
 
 # CLIENTS
 def liste_clients(request):
@@ -44,6 +78,21 @@ def ajout_client(request):
         Client.objects.create(nom=nom, contact=contact)
         return redirect('liste_clients')
     return render(request, 'clients/ajout.html')
+def modifier_client(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    if request.method == 'POST':
+        client.nom = request.POST['nom']
+        client.contact = request.POST['contact']
+        client.save()
+        return redirect('liste_clients')
+    return render(request, 'clients/modifier.html', {'client': client})
+
+def supprimer_client(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    if request.method == 'POST':
+        client.delete()
+        return redirect('liste_clients')
+    return render(request, 'clients/supprimer.html', {'client': client})
 
 # ACHATS
 def liste_achats(request):
@@ -58,6 +107,22 @@ def ajout_achat(request):
         return redirect('liste_achats')
     clients = Client.objects.all()
     return render(request, 'achats/ajout.html', {'clients': clients})
+def modifier_achat(request, pk):
+    achat = get_object_or_404(Achat, pk=pk)
+    if request.method == 'POST':
+        achat.client = Client.objects.get(id=request.POST['client'])
+        achat.date = request.POST['date']
+        achat.save()
+        return redirect('liste_achats')
+    clients = Client.objects.all()
+    return render(request, 'achats/modifier.html', {'achat': achat, 'clients': clients})
+
+def supprimer_achat(request, pk):
+    achat = get_object_or_404(Achat, pk=pk)
+    if request.method == 'POST':
+        achat.delete()
+        return redirect('liste_achats')
+    return render(request, 'achats/supprimer.html', {'achat': achat})
 
 # PANIERS
 def liste_paniers(request):
@@ -76,6 +141,24 @@ def ajout_panier(request):
     clients = Client.objects.all()
     produits = Produit.objects.all()
     return render(request, 'paniers/ajout.html', {'clients': clients, 'produits': produits})
+def modifier_panier(request, pk):
+    panier = get_object_or_404(PanierClient, pk=pk)
+    if request.method == 'POST':
+        panier.client = Client.objects.get(id=request.POST['client'])
+        panier.produit = Produit.objects.get(id=request.POST['produit'])
+        panier.quantite = request.POST['quantite']
+        panier.save()
+        return redirect('liste_paniers')
+    clients = Client.objects.all()
+    produits = Produit.objects.all()
+    return render(request, 'paniers/modifier.html', {'panier': panier, 'clients': clients, 'produits': produits})
+
+def supprimer_panier(request, pk):
+    panier = get_object_or_404(PanierClient, pk=pk)
+    if request.method == 'POST':
+        panier.delete()
+        return redirect('liste_paniers')
+    return render(request, 'paniers/supprimer.html', {'panier': panier})
 
 # TRANSACTIONS
 def liste_transactions(request):
@@ -93,6 +176,23 @@ def ajout_transaction(request):
     achats = Achat.objects.all()
     return render(request, 'transactions/ajout.html', {'achats': achats})
 
+def modifier_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+    if request.method == 'POST':
+        transaction.achat = Achat.objects.get(id=request.POST['achat'])
+        transaction.montant = request.POST['montant']
+        transaction.type_paiement = request.POST['type_paiement']
+        transaction.save()
+        return redirect('liste_transactions')
+    achats = Achat.objects.all()
+    return render(request, 'transactions/modifier.html', {'transaction': transaction, 'achats': achats})
+
+def supprimer_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+    if request.method == 'POST':
+        transaction.delete()
+        return redirect('liste_transactions')
+    return render(request, 'transactions/supprimer.html', {'transaction': transaction})
 # FACTURES
 def liste_factures(request):
     factures = Facture.objects.all()
