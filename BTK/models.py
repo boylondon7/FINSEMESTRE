42e3627886b2d3ab019542a729ee31f7bf9439e3
boylondon7 <gagalostone@gmail.com>
2025-02-23 22:02:43 +0000

    modification sur models.py ajout des commentaires pour les modeles

--- BTK/models.py
+++ BTK/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-
+#Model pour la class Categorie
 class Categorie(models.Model):
     nom = models.CharField(max_length=255)
 
-   
 
+
+#Model pour la class Produit
 class Produit(models.Model):
     nom = models.CharField(max_length=255)
     prix = models.FloatField()
@@ -17,23 +18,27 @@ class Produit(models.Model):
 
     
 
+#Model pour la class Client
 class Client(models.Model):
     nom = models.CharField(max_length=255)
     contact = models.CharField(max_length=255)
 
 
+#Model pour la class Achat
 class Achat(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='achats')
     date = models.DateField()
 
    
 
+#Model pour la class PanierClient
 class PanierClient(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='panier_clients')
     produit = models.ForeignKey(Produit, on_delete=models.CASCADE, related_name='panier_clients')
     quantite = models.IntegerField()
 
-    
+
+#Model pour la class Transaction
 
 class Transaction(models.Model):
     achat = models.ForeignKey(Achat, on_delete=models.CASCADE, related_name='transactions')
@@ -42,6 +47,7 @@ class Transaction(models.Model):
 
     
 
+#Model pour la class Facture
 class Facture(models.Model):
     achat = models.OneToOneField(Achat, on_delete=models.CASCADE, related_name='facture')
     date = models.DateField()
