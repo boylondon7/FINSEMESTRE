931527d405d5f09de981aa9a64b0a92bace300bb
boylondon7 <gagalostone@gmail.com>
2025-02-25 09:59:23 +0000

    modification sur les achats niveau modifications

--- BTK/urls.py
+++ BTK/urls.py
@@ -6,45 +6,74 @@ urlpatterns = [
     #Accueil
     path('', views.index, name="index"), 
     # CATEGORIES
+    #
     path('categories/', views.liste_categories, name="liste_categories"),
+    #
     path('categories/nouveau/', views.ajout_categorie, name="ajout_categorie"),
+    #
     path('categories/modifier/<int:pk>/', views.modifier_categorie, name='modifier_categorie'),
+    #
     path('categories/supprimer/<int:pk>/', views.supprimer_categorie, name='supprimer_categorie'),
 
     # PRODUITS
+    #
     path('produits/', views.liste_produits, name="liste_produits"),
+    #
     path('produits/nouveau/', views.ajout_produit, name="ajout_produit"),
+    #
     path('produits/modifier/<int:pk>/', views.modifier_produit, name='modifier_produit'),
+    #
     path('produits/supprimer/<int:pk>/', views.supprimer_produit, name='supprimer_produit'),
 
     # CLIENTS
+    #
     path('clients/', views.liste_clients, name="liste_clients"),
+    #
     path('clients/nouveau/', views.ajout_client, name="ajout_client"),
+    #
     path('clients/modifier/<int:pk>/', views.modifier_client, name='modifier_client'),
+    #
     path('clients/supprimer/<int:pk>/', views.supprimer_client, name='supprimer_client'),
 
     # ACHATS
+    #
     path('achats/', views.liste_achats, name="liste_achats"),
+    #
     path('achats/nouveau/', views.ajout_achat, name="ajout_achat"),
+    #
     path('achats/modifier/<int:pk>/', views.modifier_achat, name='modifier_achat'),
+    #
     path('achats/supprimer/<int:pk>/', views.supprimer_achat, name='supprimer_achat'),
 
     # PANIERS
+    #
     path('panier/', views.liste_paniers, name="liste_paniers"),
+    #
     path('panier/ajout/', views.ajout_panier, name="ajout_panier"),
+    #
     path('panier/modifier/<int:pk>/', views.modifier_panier, name='modifier_panier'),
+    #
     path('panier/supprimer/<int:pk>/', views.supprimer_panier, name='supprimer_panier'),
 
     # TRANSACTIONS
+    #
     path('transactions/', views.liste_transactions, name="liste_transactions"),
+    #
     path('transactions/nouveau/', views.ajout_transaction, name="ajout_transaction"),
+    #
     path('transactions/modifier/<int:pk>/', views.modifier_transaction, name='modifier_transaction'),
+    #
     path('transactions/supprimer/<int:pk>/', views.supprimer_transaction, name='supprimer_transaction'),
     
 
     # FACTURES
+    #
     path('factures/', views.liste_factures, name="liste_factures"),
+    #
     path('factures/nouveau/', views.ajout_facture, name="ajout_facture"),
+    
+
+         
 
 
 ]
